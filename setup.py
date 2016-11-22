fe76c8c16a35aa3e62fc2151d7817c1793c66b08
Michael Koutroumpas <m.koutroumpas@ed.ac.uk>
2016-11-22 12:29:20 +0200

    Remove postgres (needed by geoserver/geonetwork) and sqlite (obsolete)

--- setup.py
+++ setup.py
@@ -13,9 +13,7 @@ setup(
     },
 
     install_requires=['bottle==0.11.4',
-                      'WebTest==2.0.20',
-                      'psycopg2==2.5.3',
-                      'pysqlite==2.6.3'],
+                      'WebTest==2.0.20'],
     zip_safe=True,
     entry_points={
         'console_scripts': [
